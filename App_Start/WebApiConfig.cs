d4c9f51c69cf25cd433601cff4824268cd0b9cc7
Mehran-Pasha <mehranpasha2010@gmail.com>
2014-06-08 19:05:50 +0500

    update

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace TermProject
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                //routeTemplate: "api/{controller}/{name}/{paswd}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+                config.Routes.MapHttpRoute(
+                name: "GetLogin",
+                routeTemplate: "api/{controller}/{action}/{username}/{password}"
+            );
+                
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+            
+        }
+    }
+}
